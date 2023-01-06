fb35111798fcca6e11a0b203af19c9f8e0e4eb4d
rosec <roseckasara@gmail.com>
2023-01-06 16:45:38 +0100

    funkční cesta

--- Public/index.php
+++ Public/index.php
@@ -1,3 +1,48 @@
 <?php
 
-echo 'REQUEST URL= "'.$_SERVER['QUERY_STRING'].'"'; 
+/**
+ * Front controller
+ *
+ * PHP version 5.4
+ */
+require '../APP/Controllers/Posts.php';
+
+/**
+ * Routing
+ */
+require '../Core/Router.php';
+
+$router = new Router();
+
+// Add the routes
+$router->add('', ['controller' => 'Home', 'action' => 'index']);
+$router->add('posts', ['controller' => 'Posts', 'action' => 'index']);
+$router->add('posts/new', ['controller' => 'Posts', 'action' => 'new']);
+$router->add('{controller}/{action}');
+$router->add('admin/{action}/{controller}');
+
+// Display the routing table
+//echo '<pre>';
+//var_dump($router->getRoutes());
+//echo '</pre>';
+
+/*
+// Display the routing table
+echo '<pre>';
+//var_dump($router->getRoutes());
+echo htmlspecialchars(print_r($router->getRoutes(), true));
+echo '</pre>';
+// Match the requested route
+$url = $_SERVER['QUERY_STRING'];
+
+
+if ($router->match($url)) {
+    echo '<pre>';
+    var_dump($router->getParams());
+    echo '</pre>';
+} else {
+    echo "No route found for URL '$url'";
+}
+*/
+
+$router->dispatch( '{controller}/{action}');
