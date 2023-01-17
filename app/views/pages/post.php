5c3b717cb478d97148f84922c9c999edb5288dbe
rosec <roseckasara@gmail.com>
2023-01-17 17:12:56 +0100

    fungující tlačítka pokračovat

--- app/views/pages/post.php
+++ app/views/pages/post.php
@@ -1,5 +1,6 @@
 <?php require APPROOT.'/views/inc/header.php'?>
-<p><?php echo $data['id'];?></p>
+<!-- <p><?php echo $data['id'];?></p> -->
+<?php $temp = "pages/post/".$data['id']+1;?>
 <div class="post">
     <h1 class="nameOfPost"><?php echo $data['nameOfPost'] ?> </h1>
     <p><?php echo $data['text'] ?> </p>
@@ -7,13 +8,13 @@
     <!-- todo button next page-->
     <div class="tlacitkoNext">
         <form action="index.php" method="get">
-        <input type="submit" value="1" name="btn">
+        <input type="submit" value='<?php echo $temp ?>' name="url">
         </form>
     </div>    
 </div>
 <?php 
     if(isset($_GET['btn']) && $_GET['btn'] == "další"){
-        post(1);
+        //post(1);
     }
 ?>
 
